70f9d8148cf23a8cf5df24c52f58d1748d61ba96
Quinten Verniers <quintenverniers@gmail.com>
2022-01-22 18:59:47 +0100

    minor css changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { useEffect } from "react";
 import Soundboard from "./components/Soundboard";
+import { title } from "./sounds";
 
 function App() {
+  useEffect(() => {
+    document.title = title;
+  }, []);
+
   return (
     <div className="App">
       <Soundboard />
